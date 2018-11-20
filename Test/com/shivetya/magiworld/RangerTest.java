637ee2a214cdad148d7c8439eff4586b44a2fc04
Shivetya <gthuillier2@gmail.com>
2018-11-20 10:47:57 +0100

    RangerTest coded with 2 tests : test for basicAttack and damaged, test for specialAttack

--- Test/com/shivetya/magiworld/RangerTest.java
+++ Test/com/shivetya/magiworld/RangerTest.java
@@ -1,10 +1,21 @@
 package com.shivetya.magiworld;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertEquals;
 
-import org.junit.Test;
+class RangerTest {
 
-public class RangerTest {
+    @Test
+    void Given_BasicAttack_When_DamagedOnHimself_Then_DamagesOnHimselfequalsToAgility(){
+        Ranger test = new Ranger(30,10,10,10);
+        test.basicAttack(test);
+        assertEquals("Les dégats doivent être égaux à l'agilité, la vie du personnage doit descendre",140, test.getLife());
+    }
 
-
-
+    @Test
+    void Given_SpecialAttack_When_SpecialAttack_Then_DoubleAgility(){
+        Ranger test = new Ranger(30,10,10,10);
+        test.specialAttack(test);
+        assertEquals("L'agilité doit doubler avec l'attaque spéciale", 25, test.getAgility());
+    }
 }
