b8bf6b6d8754b8dd3b88dd6126d68f5d54d729c6
Shivetya <gthuillier2@gmail.com>
2018-11-20 10:49:11 +0100

    Methods names modified in RangerTest

--- Test/com/shivetya/magiworld/RangerTest.java
+++ Test/com/shivetya/magiworld/RangerTest.java
@@ -6,14 +6,14 @@ import static org.junit.Assert.assertEquals;
 class RangerTest {
 
     @Test
-    void Given_BasicAttack_When_DamagedOnHimself_Then_DamagesOnHimselfequalsToAgility(){
+    void Given_BasicAttack_When_DamagedOnHimself_Then_DamagesOnHimselfEqualsToAgility(){
         Ranger test = new Ranger(30,10,10,10);
         test.basicAttack(test);
         assertEquals("Les dégats doivent être égaux à l'agilité, la vie du personnage doit descendre",140, test.getLife());
     }
 
     @Test
-    void Given_SpecialAttack_When_SpecialAttack_Then_DoubleAgility(){
+    void Given_SpecialAttack_When_SpecialAttack_Then_AddHalfLevelToAgility(){
         Ranger test = new Ranger(30,10,10,10);
         test.specialAttack(test);
         assertEquals("L'agilité doit doubler avec l'attaque spéciale", 25, test.getAgility());
