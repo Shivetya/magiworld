bdcd6dc0d22ba272d363c8c95001a3d5f21a57dc
Shivetya <gthuillier2@gmail.com>
2018-11-21 10:52:25 +0100

    add javaDoc

--- src/com/shivetya/magiworld/Character.java
+++ src/com/shivetya/magiworld/Character.java
@@ -6,6 +6,7 @@ interface Character {
      * basicAttack & specialAttack are two actions possible for each character : they have the target Character in parameter.
      * damaged() allow to decrease life of the character, depend to the damages
      * isDead allow to know if the life's character equals 0
+     * getBasicDamage & getSpecialDamage return the exact damages done
      */
     void basicAttack(Character attacked);
     void specialAttack(Character attacked);
--- src/com/shivetya/magiworld/Player.java
+++ src/com/shivetya/magiworld/Player.java
@@ -22,6 +22,12 @@ public class Player {
         character = character;
     }
 
+    /**
+     * Methods Creator() allow to create Character to the player : it use methods "askX()" to have specifications of the
+     * character to create.
+     * if specifications are not in the normal range, program throw BadSpecException
+     * @return the character created.
+     */
     private Character creator(){
 
         int choice = askCharacter();
@@ -52,12 +58,21 @@ public class Player {
         return character;
     }
 
+    /**
+     * Performs basic attack from the character to the character attacked. Display texts with actions done and damages done
+     * @param attacked : character attacked
+     */
     void basicAttack(Player attacked){
         character.basicAttack(attacked.getCharacter());
         System.out.println(name + character.displayBasicAttack());
         System.out.println(attacked.getName() + " perd " + character.getBasicDamage() + " points de vie.");
     }
 
+    /**
+     * perform special Attack from the character to the character attacked
+     * IMPORTANT : if the character who perform this attack is Magus or Ranger, attacked must be himself !!
+     * @param attacked is the target of the attack
+     */
     void specialAttack(Player attacked){
         character.specialAttack(attacked.getCharacter());
         System.out.println(name + character.displaySpecialAttack());
